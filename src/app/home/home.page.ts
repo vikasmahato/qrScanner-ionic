d739137ac2dd45ef4c5f9827f0a0562d91807a75
Vikas Mahato <vikasmahato0@gmail.com>
2020-04-23 23:51:42 +0530

    Added basic function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,59 @@ export class HomePage {
   QRSCANNED_DATA: string;
   isOn = false;
   scannedData: {};
-  
-  constructor(public qrScanCtrl: QRScanner) {}
+
+  constructor(public qrScanCtrl: QRScanner, public toastController: ToastController) {}
+
+  async presentToast(text: string) {
+    const toast = await this.toastController.create({
+      message: text,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  goToQrScan() {
+    this.presentToast("goToQrScan");
+    this.qrScanCtrl.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+          this.isOn = true;
+
+
+          // start scanning
+          const scanSub = this.qrScanCtrl.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+            this.presentToast('Scanned something' + text);
+            this.isOn = false;
+
+            this.QRSCANNED_DATA = text;
+            if (this.QRSCANNED_DATA !== '') {
+              this.closeScanner();
+              scanSub.unsubscribe();
+            }
+
+          });
+          this.qrScanCtrl.show();
+
+        } else if (status.denied) {
+          console.log('camera permission denied');
+          this.presentToast('Camera permission denied');
+          this.qrScanCtrl.openSettings();
+        } else {
+        }
+      })
+      .catch((e: any) => this.presentToast(e));
+  }
+
+  closeScanner() {
+    this.isOn = false;
+    this.qrScanCtrl.hide();
+    this.qrScanCtrl.destroy();
+  }
+
+  goToCreateCode() {
+    this.presentToast("create codes");
+  }
 
 }
